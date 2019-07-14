9b2145330eaad9f3a215bfa18ab06ceb57d83d8e
Jen <jenniferanndcb@gmail.com>
2019-07-14 21:11:32 +0100

    changed url search results

--- lib/meal_finder/cli.rb
+++ lib/meal_finder/cli.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 module MealFinder 
   class CLI
 
--- lib/meal_finder/scraper.rb
+++ lib/meal_finder/scraper.rb
@@ -1,13 +1,18 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
 module MealFinder 
   class Scraper 
 
     BASE_URL = "https://www.bbcgoodfood.com" 
 
     def self.scrape_courses
-      html = open('https://www.bbcgoodfood.com/search/recipes?query=')
+      html = open('https://www.bbcgoodfood.com/search/recipes?query=course')
       doc = Nokogiri::HTML(html)
-  
+      
       doc.css('ul#facetapi-facet-search-apinode-index-block-field-bbcgf-course li.leaf').each do |course| 
+        
         new_course = Course.new 
         new_course.name = course.css("a :first-child").text.gsub(/Apply|filter/, "").strip
          
@@ -21,8 +26,9 @@ module MealFinder
     def self.scrape_recipes(selected_course) 
 
     doc = Nokogiri::HTML(open(selected_course.url))
-
-      doc.css(".view-content h3").each do |recipes|  
+    #binding.pry
+      doc.css("#search-results h3").each do |recipes| 
+        
         new_recipe = Recipes.new
         new_recipe.name = recipes.css("a").text.strip
         new_recipe.url = BASE_URL + recipes.css("a")[0]["href"]
